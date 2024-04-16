1fabceeec113d7bd735f5599ef2e9949423ebc3a
Tom Van den Bulck <159095052+tvanden3@users.noreply.github.com>
2024-04-16 16:59:06 +0000

    Adding a PUT method to allow changes to be made to a movie
    I made changes to movies.py and main.py to add the PUT method and make it update or change movie information. Ran a few tests to make sure it worked.
    
    This addresses Issue #2

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI 
 from typing import Union
 from movies import Movies
+from movies import Movie
 
 mApp = FastAPI()
 
@@ -8,7 +9,18 @@ movies = Movies("./movies.txt")
 
 @mApp.get("/movie/{movie_id}")
 def get_movie_by_name(movie_id: str):
-    if 0 <= int(movie_id) < len(movies._movies):
-        return movies._movies[int(movie_id)]
-    else:
-        return None
+    for i in range(0,len(movies._movies),1):
+        if movies._movies[i]['id'] == int(movie_id):
+            return movies._movies[int(i)]
+
+@mApp.put("/movie/{movie_id}")
+def update_movie(movie_id: int, new_movie: Movie) -> Union[Movie, None]:
+    for i in range(0,len(movies._movies),1):
+        if movies._movies[i]['id'] == movie_id:
+            movies._movies[i]['name'] = new_movie.name
+            movies._movies[i]['cast'] = new_movie.cast
+            return{
+                  "id" : new_movie.id,
+                  "name": new_movie.name,
+                  "cast": new_movie.cast
+        }
