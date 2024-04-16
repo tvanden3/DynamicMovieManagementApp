bcf23e14a5ac5497a8d7cb0406aa3dfb59761309
tvanden3 <159095052+tvanden3@users.noreply.github.com>
2024-04-16 15:20:32 -0400

    Revert "Fetching a movie from fast API based on Movie ID"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,15 @@
 //import logo from './logo.svg';
 import { TextField, List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import './App.css';
-import {Movie} from '@mui/icons-material';
-
-
 
 function App() {
-  const [movieId, setMovieId] = useState("1")
-  const [movie, setMovie] = useState(null)
-
-  useEffect(() => {
-    if(movieId == ""){
-      setMovie(null);
-    }
-    else{
-      fetch('http://localhost:8000/movie/${movieId}')
-      .then( result => result.json())
-      .then( result => {
-        console.log('http://localhost:8000/movie/${movieId}')
-        setMovie(result);
-      });
-    }
-    console.log(movieId);
-  }, [movieId] );
-
-  useEffect(() => {
-    console.log(movie);
-  }, [movie]);
-
-
+  const [movieId, setMovieId] = useState("5")
   return (
     <div className="App">
       <header className="App-header">
         <TextField id="outlined-basic" label="Movie Id"
-        variant ="outlined" color="warning" focused value={movieId}
+        variant ="outlined" color="warning" focuse value={movieId}
         onChange={e=> setMovieId(e.target.value)}
         />
       </header>
